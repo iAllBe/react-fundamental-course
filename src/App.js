5d368d16734fef0b26f367c416d1fbe4e0f2b119
Dmitriy Ridnyy <allbegj@gmail.com>
2026-03-14 14:02:45 +0500

    add PostItem.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import PostItem from "./components/PostItem";
+import {useState} from "react";
+
 export default function App() {
 
     return (
-        <>
-        </>
+        <div className="App">
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+        </div>
     );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,13 @@
+export default function PostItem() {
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>1. Javascript</strong>
+                <div>Javascript - язык программирования</div>
+            </div>
+            <div className="post__btns">
+                <button>Удалить</button>
+            </div>
+        </div>
+    );
+};
