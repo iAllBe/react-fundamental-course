aef80089c9677b3bd391edcea2f472a9a8093e0d
Dmitriy Ridnyy <allbegj@gmail.com>
2026-03-16 11:19:54 +0500

    fix PostForm.jsx

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 
 export default function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'JS', body: 'Desc'},
-        {id: 2, title: 'JS 2', body: 'Desc 2'},
-        {id: 3, title: 'JS 3', body: 'Desc 3'},
-    ]);
+    const [posts, setPosts] = useState([{id: 1, title: 'JS', body: 'Desc'}, {
+        id: 2,
+        title: 'JS 2',
+        body: 'Desc 2'
+    }, {id: 3, title: 'JS 3', body: 'Desc 3'},]);
 
-    const [post, setPost] = useState({title: '', body: ''});
-    console.log(post);
-
-    function addNewPost(e) {
-        e.preventDefault();
-        setPosts([...posts, {...post, id: Date.now()}]);
-        setPost({title: '', body: ''});
+    function createPost(newPost) {
+        setPosts([...posts, newPost])
     }
 
-    return (
-        <div className="App">
-            <PostForm post={post} setPost={setPost} addNewPost={addNewPost}/>
-            <PostList posts={posts} title="Список постов"/>
-        </div>
-    );
+    return (<div className="App">
+            <PostForm
+                posts={posts}
+                create={createPost}
+            />
+            <PostList
+                posts={posts}
+                title="Список постов"
+            />
+        </div>);
 }
